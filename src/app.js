49702af537e92c451dded9b9f93c5fc4d3b5b509
Otabek Toshtemirov <47772571+OtabekToshtemirov@users.noreply.github.com>
2025-01-26 16:23:23 +0500

    Restrict CORS to specific origin for enhanced security

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const path = require('path')
 const port = PORT || 5000
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: "https://ijarafrontreact.vercel.app/" }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan())
